5465be0641753e092fd20c2e56c1e7c9ae794adf
muhammadsaad0077 <saaditanoli12@gmail.com>
2025-07-30 22:42:04 +0500

    Remove Duplicates

---
+++ removeduplicate.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int arr[8] = {1, 1, 2, 2, 2, 3, 4, 4};
+	int x = 0;
+	
+	
+	
+	for(int i = 0; i < 8; i++){
+		if(arr[i] > arr[x]){
+			x++;
+			arr[x] = arr[i];
+		}
+	}
+	
+	for(int i = 0; i < 8; i++){
+		cout<<arr[i]<<endl;
+	}
+	
+	cout<<x+1;
+}
