8fac7f330e5f2dccd6016618f01a28736a3aa449
muhammadsaad0077 <saaditanoli12@gmail.com>
2025-08-23 16:10:07 +0500

    linked list

---
+++ linkedList.cpp
@@ -0,0 +1,92 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+class LinkedList{
+
+public:
+	int size;
+	LinkedList(): size(0){
+	}
+struct Node{
+    int val;
+    Node* next;
+
+    Node(int v): val(v), next(nullptr){
+    }
+};
+
+	Node* head;
+
+void addAtHead(int val){
+    Node* newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+    size++;
+}
+
+void addAtTail(int val){
+    Node* newNode = new Node(val);
+    
+    if(head == nullptr){
+        head = newNode;
+    }
+    else{
+        Node* current = head;
+       while(current->next != nullptr){
+        current = current->next;
+        }
+        current->next = newNode;
+    }
+    
+    size++;
+
+}
+
+void addAtIndex(int index, int val){
+	int count = 0;
+	Node* current = head;
+	
+	if(index == 0){
+		addAtHead(val);
+	}
+	else if(index > size - 1){
+		addAtTail(val);
+	}
+	else{
+		while(count != index - 1){
+			current = current->next;
+			count++;
+		}
+	
+		Node* newNode = new Node(val);
+		newNode->next = current->next;
+		current->next = newNode;	
+	}	
+	size++;
+	
+}
+ void printList(){
+ 	Node* current = head;
+    
+    while(current != nullptr){
+        cout<<current->val<<endl;
+        current = current->next;
+    }
+ }
+};
+
+
+int main() {
+    LinkedList* list = new LinkedList;
+    list->addAtTail(5);
+    list->addAtTail(15);
+    list->addAtTail(20);
+    
+    list->addAtHead(0);
+
+    list->printList();
+    
+
+    return 0;
+}
