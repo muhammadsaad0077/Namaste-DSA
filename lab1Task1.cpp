08c75f355a6d64b797d0826014f88ae2e527805f
muhammadsaad0077 <saaditanoli12@gmail.com>
2025-08-26 15:02:48 +0500

    remove_1

--- lab1Task1.cpp
+++ lab1Task1.cpp
@@ -23,18 +23,67 @@ void removeDuplicates(int* arr, int &size){
     size = x;
 }
 
-void printArray(int* arr, int size){
+void countElemFrequency(int* arr, int size, int* unique, int* freq, int &uniqueCount){
+    bool checked[size] = {false};
+    int frequency = 0;
+    uniqueCount = 0;
+    
     for(int i = 0; i < size; i++){
-        cout<<arr[i]<<" ";
+        if(checked[i]){
+            continue;
+        }
+        
+        else{
+        
+        frequency = 1;
+            
+            for(int j = i + 1; j < size; j++){
+                if(arr[i] == arr[j]){
+                    frequency++;
+                    checked[j] = true;
+                }
+            }
+            
+            unique[uniqueCount] = arr[i];
+            freq[uniqueCount] = frequency;
+            uniqueCount++;
+        }
+    }
+}
+
+void frequencySorted(int* unique, int* freq, int uniqueCount){
+    for(int i = 0; i < uniqueCount - 1; i++){
+        for(int j = 0; j < uniqueCount - 1 - i; j++){
+            if(freq[j] == freq[j+1]){
+                if(unique[j] > unique[j+1]){
+                    swap(unique[j], unique[j+1]);
+                    swap(freq[j], freq[j+1]);
+                }
+            }
+            else if(freq[j] < freq[j+1]){
+                swap(unique[j], unique[j+1]);
+                swap(freq[j], freq[j+1]);
+            }
+        }
+    }
+}
+
+void displayArr(int* arr, int* unique, int* freq, int size, int uniqueCount){
+    cout<<"Frequences:"<<endl;
+    for(int i = 0; i < uniqueCount; i++){
+        cout<<unique[i]<<" -> "<<freq[i]<<" times"<<endl;
     }
 }
 
 int main() {
-    int size = 5;
-    int arr[size] = {6, 3, 3, 6, 5};
+    int arr[] = {7, 3, 5, 7, 3};
+    int size = sizeof(arr) / sizeof(int);
+    int unique[size], freq[size];
+    int uniqueCount;
     
-    removeDuplicates(arr, size);
-    printArray(arr, size);
-
-    return 0;
+    countElemFrequency(arr, size, unique, freq, uniqueCount);
+    
+    frequencySorted(unique, freq, uniqueCount);
+    
+    displayArr(arr, unique, freq, size, uniqueCount);
 }
